9a931576c5338c06206b3bc7e26e653a4607e3ea
MD MOFID UMAR <147265098+mofidumar15@users.noreply.github.com>
2026-08-14 20:15:41 +0530

    Create HeroSection.js

---
+++ components/HeroSection.js
@@ -0,0 +1,15 @@
+export default function HeroSection() {
+  return (
+    <section className="py-20 text-center">
+      <h1 className="text-6xl font-bold mb-6">
+        Smart Medicine Safety Assistant
+      </h1>
+
+      <p className="text-xl max-w-3xl mx-auto">
+        AI-powered drug interaction analysis,
+        prescription understanding and medicine
+        safety guidance.
+      </p>
+    </section>
+  );
+}
