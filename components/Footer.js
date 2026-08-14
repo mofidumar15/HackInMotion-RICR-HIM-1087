21f288b0130d0b428bb6bb4bfaabbf04435fa90e
MD MOFID UMAR <147265098+mofidumar15@users.noreply.github.com>
2026-08-14 20:16:32 +0530

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <footer className="mt-20 py-8 text-center border-t">
+      <p>
+        CureDrug © 2026
+      </p>
+
+      <p className="text-sm mt-2">
+        Smart Medicine Safety Assistant
+      </p>
+    </footer>
+  );
+}
